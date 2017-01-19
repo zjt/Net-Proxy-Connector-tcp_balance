6f1d36449a039110c0ed3ecc79a2110fbd139c6e
Jesse Thompson <jesse.thompson@wisc.edu>
2017-01-19 11:02:31 -0600

    added some additional modules needed for release, and made the container ephemeral

--- docker-dzil.sh
+++ docker-dzil.sh
@@ -14,17 +14,24 @@ RUN cpanm -n Dist::Zilla::Plugin::PodWeaver \
              Dist::Zilla::Plugin::Test::Compile \
              Dist::Zilla::Plugin::Test::Kwalitee \
              Dist::Zilla::Plugin::Test::Perl::Critic \
-             Dist::Zilla::Plugin::MinimumPerl
+             Dist::Zilla::Plugin::MinimumPerl \
+             Test::Pod \
+             Test::Pod::Coverage \
+             Test::Kwalitee \
+             Pod::Coverage::TrustPod
 Dockerfile
 
 # build the docker image from the Dockerfile
+echo "$0 ... building docker image"
 docker build -t dzil-npctb .
 
 # run the container from the built image
 # - mount the current directory inside the container
 # - pass into the container whatever was passed in to this script
+# - delete the container when done
 # effectively this is running "dzil @args" inside the container
-docker run -v $PWD:/project dzil-npctb $@
+echo "$0 ... running docker container"
+docker run --rm -v $PWD:/project dzil-npctb $@
 
 # clean up the temporary Dockerfile
 rm Dockerfile
